f3dace69cae42941c7ae434ab0382da7d4623501
Kromylian <oscarcastillo212002@gmail.com>
2026-02-20 00:59:56 -0500

    creacion de peticion de api por  medio de requisito post de login validado por el backend con un usuario y contrase;a estaticos

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,49 +1,84 @@
 import { Component, signal } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-  loginForm: FormGroup;
+  //loginForm: FormGroup;
   isLoading = signal(false);
   errorMessage = signal('');
 
-  constructor(private fb: FormBuilder, private router: Router) {
-    this.loginForm = this.fb.group({
-      usuario: ['', [Validators.required, Validators.minLength(3)]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      recuerda: [false]
+  //vaariables de solicitud
+  public solicitud:any;
+  public mensaje: string = '';
+
+  constructor(private auth: Auth) {
+    this.solicitud = {
+      email:'',
+      password:''
+    }
+  }
+
+  // constructor(private fb: FormBuilder, private router: Router) {
+  //   this.loginForm = this.fb.group({
+  //     usuario: ['', [Validators.required, Validators.minLength(3)]],
+  //     password: ['', [Validators.required, Validators.minLength(6)]],
+  //     recuerda: [false]
+  //   });
+  // }
+
+  OnSubmit() {
+    console.log(this.solicitud);
+
+    this.auth.login(this.solicitud).subscribe({
+      next: (response: any) => {
+        this.mensaje = response.message;
+
+        // guardar token si existe
+        if(response.token){
+          localStorage.setItem('token', response.token);
+        }
+
+        // redireccionar si login correcto
+        if(response.success){
+          window.location.href = '/inicio';
+        }
+      },
+      error: (error) => {
+        this.mensaje = error.error.message;
+      }
     });
   }
 
-  onLogin() {
-    if (this.loginForm.valid) {
-      this.isLoading.set(true);
-      this.errorMessage.set('');
+  // onLogin() {
+  //   if (this.loginForm.valid) {
+  //     this.isLoading.set(true);
+  //     this.errorMessage.set('');
 
-      const credenciales = this.loginForm.value;
-      //Proabndo que llegan las credenciales
-      console.log('Intentando iniciar sesión con:', credenciales);
+  //     const credenciales = this.loginForm.value;
+  //     //Proabndo que llegan las credenciales
+  //     console.log('Intentando iniciar sesión con:', credenciales);
 
-      // Simular llamada al servidor
-      setTimeout(() => {
-        // Validación simple de ejemplo
-        if (credenciales.usuario === 'admin' && credenciales.password === 'admin123') {
-          this.loginForm.reset();
-          // Va a la página de inicio
-          this.router.navigate(['/inicio']);
-        } else {
-          this.errorMessage.set('Usuario o contraseña incorrectos');
-        }
-        this.isLoading.set(false);
-      }, 1500);
-    }
-  }
-  }
+  //     // Simular llamada al servidor
+  //     setTimeout(() => {
+  //       // Validación simple de ejemplo
+  //       if (credenciales.usuario === 'admin' && credenciales.password === 'admin123') {
+  //         this.loginForm.reset();
+  //         // Va a la página de inicio
+  //         this.router.navigate(['/inicio']);
+  //       } else {
+  //         this.errorMessage.set('Usuario o contraseña incorrectos');
+  //       }
+  //       this.isLoading.set(false);
+  //     }, 1500);
+  //   }
+  // }
+}
 
---
+++ src/app/services/auth.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+  private apiUrl = 'http://localhost/api/login';
+
+  constructor(private http: HttpClient) {}
+
+  login(credentials: any) {
+    return this.http.post(this.apiUrl, credentials);
+  }
+}
